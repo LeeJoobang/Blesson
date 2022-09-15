fd933fd977c5839c6f1e8bab94b0defb6ebc3f4d
Joobang <hii5074@gmail.com>
2022-09-15 14:22:18 +0900

    "[add] 학생 저장 뷰 - image 라운드 적용"

--- Blessen/Blessen/Scene/Main_Register/RegisterImageCell.swift
+++ Blessen/Blessen/Scene/Main_Register/RegisterImageCell.swift
@@ -6,9 +6,18 @@ class RegisterImageCell: BaseTableViewCell {
     let idImageView: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "Test.png")!
+        image.backgroundColor = .red
+        image.layer.cornerRadius = 50 / 2
+        image.layer.masksToBounds = true
         image.sizeToFit()
         return image
     }()
+    
+    // MARK: 이미지 라운드 적용
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        idImageView.layer.cornerRadius = idImageView.frame.width / 2
+    }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -27,10 +36,10 @@ class RegisterImageCell: BaseTableViewCell {
     override func setConstraints() {
         
         idImageView.snp.makeConstraints { make in
-            make.top.equalTo(self.safeAreaLayoutGuide.snp.top)
-            make.leading.equalTo(20)
-            make.height.equalTo(contentView.snp.height)
-            make.trailing.equalTo(-20)
+            make.top.equalTo(self.contentView).multipliedBy(10)
+            make.width.equalTo(self.contentView).multipliedBy(0.2)
+            make.height.equalTo(idImageView.snp.width)
+            make.centerX.equalTo(self.contentView.snp.centerX)
         }
         
     }
